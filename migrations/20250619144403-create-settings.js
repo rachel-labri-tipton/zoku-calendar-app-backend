b755e129ba2a01c8237a1342ff8af6303289a872
Rachel Tipton <rachelmtipton@gmail.com>
2025-06-19 17:15:14 +0200

    create migration for the settings model

---
+++ migrations/20250619144403-create-settings.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Settings', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      theme: {
+        type: Sequelize.STRING,
+        defaultValue: 'light'
+      },
+      notifications: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      language: {
+        type: Sequelize.STRING,
+        defaultValue: 'en'
+      },
+      timezone: {
+        type: Sequelize.STRING,
+        defaultValue: 'UTC'
+      },
+      startOfWeek: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Settings');
+  }
+};
