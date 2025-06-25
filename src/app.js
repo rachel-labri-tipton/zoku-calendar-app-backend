f442c8d10bc5f89f4ad83075cb3170743bf6716a
Rachel Tipton <rachelmtipton@gmail.com>
2025-06-25 21:23:25 +0200

    adds cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const { sequelize } = require('../models');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // Replace with your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Allow cookies and authorization headers
+};
+
 // Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Import routes
@@ -21,7 +30,9 @@ app.use('/events', eventRoutes);
 app.use('/todos', todoListRoutes);
 app.use('/settings', settingsRoutes);
 app.use('/time-logs', timeLogRoutes); 
-app.use ('/categories', categoryRoutes);
+app.use('/categories', categoryRoutes);
+
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
